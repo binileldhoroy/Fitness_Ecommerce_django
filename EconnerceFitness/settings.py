ca46514c4b22787b0aa03dd211ddd7736097fefc
binil eldho roy <bi2277009@gmail.com>
2022-03-08 11:18:28 +0530

    for deploy

--- EconnerceFitness/settings.py
+++ EconnerceFitness/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ske$ik$msphn0oxq+@%k0^2sqr27^frh*on+gyucr31&wq9q(('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
