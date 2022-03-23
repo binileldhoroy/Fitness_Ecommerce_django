db1d17c131fa8b32d4ff9400064a4881e3c6837d
binil eldho roy <bi2277009@gmail.com>
2022-03-23 16:31:46 +0530

    check

--- EconnerceFitness/settings.py
+++ EconnerceFitness/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ske$ik$msphn0oxq+@%k0^2sqr27^frh*on+gyucr31&wq9q(('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
